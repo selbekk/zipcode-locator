e005235044ae6e87a7b83bd158a87204b2e71cb9
selbekk <kristofer.selbekk@bekk.no>
2015-09-16 12:22:05 -0700

    Add some basic info about the site

--- src/backend/utils/view-util.js
+++ src/backend/utils/view-util.js
@@ -2,14 +2,14 @@ let assign = require('object-assign');
 
 let defaultContext = {
     meta: {
-        baseUrl: '',
-        siteTitle: '',
+        baseUrl: 'http://localhost:4000',
+        siteTitle: 'What\'s my ZIP Code?',
         title: '',
         url: '',
         image: '',
-        description: '',
-        author: '',
-        keywords: ''
+        description: 'Wondering what the zip code is where you\'re at? We\'ll tell you!',
+        author: 'Kristofer Selbekk',
+        keywords: 'zipcode, postal code, postnummer'
     }
 };
 
